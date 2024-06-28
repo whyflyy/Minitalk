be7904af95650e7e32932926faa2c85833c27112
Joao Cavadas Ribeiro <jcavadas@c1r3s4.42porto.com>
2024-06-28 15:42:03 +0100

    Working on one char at a time, now to try to send len and malloc it on server

--- client.c
+++ client.c
@@ -6,13 +6,29 @@
 /*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 14:45:02 by jcavadas          #+#    #+#             */
-/*   Updated: 2024/06/26 14:33:21 by jcavadas         ###   ########.fr       */
+/*   Updated: 2024/06/28 15:40:08 by jcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    send_message(char *str, int size)
+void	send_size(int pid, int size)
+{
+	int	i;
+
+	i = 31;
+	while (i >= 0)
+	{
+		if (size & (1 << i))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(50);
+		i--;
+	}
+}
+
+void	send_message(int pid, char *str, int size)
 {
 	int		i;
 	int		x;
@@ -21,22 +37,27 @@ void    send_message(char *str, int size)
 	x = 0;
 	while (i < size)
 	{
-		x = 8;
-		while (x > 0)
+		x = 7;
+		while (x >= 0)
 		{
-			ft_printf("%d", (str[i] >> x) & 1); //--------- NOT WORKINGGGGGGGGG ---------------------------
+			if((str[i] >> x) & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
 			//str[i] = str[i] << 1;
 			x--;
+			usleep(50);
 		}
+		//ft_printf(" ");
 		i++;
 	}
-	
 }
 
-int     main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	char    *str;
+	char	*str;
 	int		len;
+	int		pid;
 
 	if (argc != 3)
 	{
@@ -46,7 +67,9 @@ int     main(int argc, char **argv)
 	len = 0;
 	str = argv[2];
 	len = ft_strlen(str);
-	send_message(str, len);
-	
+	pid = ft_atoi(argv[1]);
+	send_size(pid, len);
+	send_message(pid, str, len);
+
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,17 +6,42 @@
 /*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 14:45:07 by jcavadas          #+#    #+#             */
-/*   Updated: 2024/06/26 14:33:19 by jcavadas         ###   ########.fr       */
+/*   Updated: 2024/06/28 15:40:47 by jcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main(void)
+void	handler_message(int sig)
+{
+	static char		c = 0;
+	static int		i = 0;
+
+	if (sig == SIGUSR2)
+	{
+		c = (c << 1) | 1;
+		
+		//ft_printf("%c", c);
+	}
+	else
+	{
+		c = c << 1;
+		//ft_printf("%c", c);
+	}
+	i++;
+	if(i == 8)
+	{
+		ft_printf("%c", c);
+		i = 0;
+	}
+}
+
+int	main(void)
 {
 	ft_printf("PID: %d\n", getpid());
 	while (1)
 	{
-		/* code */
+		signal(SIGUSR1, handler_message);
+		signal(SIGUSR2, handler_message);
 	}
 }
